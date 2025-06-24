a6695049ee944c722f6139107dd41b76863dba0c
Bittu <ssr7a5134@gmail.com>
2025-06-24 20:59:49 +0530

    Update main.py

--- orvcord/main.py
+++ orvcord/main.py
@@ -3,32 +3,31 @@ import json
 
 path = "orvcord/feed.json"
 url = "https://flamecomics.xyz/_next/data/OG-uJ8nhcG8usaV_1hTQT/series/2/0c9db8012fbd1257.json"
-chid = "2.0"
+last_chid = "2.0"
 response = requests.get(url)
 data = response.json()
 
 with open(path, 'r') as f:
-    chd = json.load(f)["chid"]
+    chid = json.load(f)["chid"]
 
 series_detail = data['pageProps']['chapterList']
 data = []
 
 for chapter_element in series_detail:
     chapter_info = {
+"chapter": chapter_element['chapter'],
 "Id": chapter_element['chapter_id'],
 "token": chapter_element['token'],
 "title": chapter_element['title'],
-"chapter": chapter_element['chapter'],
+
 "release_date": chapter_element['release_date']
 }
-    print(chapter_info)
+    #print(chapter_info)
     if chid == chapter_element['chapter']:
         print(chapter_info, last_chid)
         data.append(chapter_info)
-
-        
+        break
     else:
         chid_last = chapter_element['chapter']
-
 with open(path, 'w') as f:
     json.dump({"chid":chid_last,"data":data}, f, indent=4)
