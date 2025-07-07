7323301fb659eeba900efb46e9a7148bcd126a19
Bittu <ssr7a5134@gmail.com>
2025-07-07 22:26:33 +0530

    Update main.py

--- orvcord/main.py
+++ orvcord/main.py
@@ -51,7 +51,7 @@ for chapter_element in series_detail:
         data.append(chapter_info)
 
         payload = {
-  "content": "||<@&1255148096430673940>||",
+  "content": "||<@&1255148011210674187>||",
   "embeds": [
     {
       "title": f"✨ Chapter {chapter_element['chapter'].replace('.0','')} - {chapter_element['title']}",
